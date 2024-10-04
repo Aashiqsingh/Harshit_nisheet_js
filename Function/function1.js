06d4929d32d4cbc3b767ce08450e9cfa6d5f83aa
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-04 12:58:49 +0530

    function

---
+++ Function/function1.js
@@ -0,0 +1,64 @@
+// without return withour argument
+function demo(){
+    console.log('This is a demo function.');
+}
+
+// demo()
+
+// without return with argument
+
+function add(a,b){
+    console.log("Addition = ",a+b);
+    
+    
+}
+
+// add(4,5)
+
+//  with return without argument
+
+function demo2(){
+
+    return "This is demo2 function"    
+}
+
+
+
+// console.log(demo2());
+
+
+// var x = demo2()
+// console.log("New function === ",x);
+
+
+// with return with argument
+
+// function mul(x,y){
+//     return x*y;
+// }
+
+// var x = mul(3,4)
+// console.log("multiply = ",x);
+
+
+// find area of circle
+// area of rectangle
+// area of square
+// prime number 
+// user number odd / even
+
+
+
+
+
+function add(x,y){
+    
+    return x+y;
+}
+    var a = parseInt(prompt("Enter 1st number "))
+
+    var b = parseInt(prompt("Enter 2nd number "))
+
+
+var x = add(a,b)
+console.log("Addition = ",x);
