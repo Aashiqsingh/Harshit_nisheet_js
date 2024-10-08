94fa775e609091707bd2b96c36e34d13c66fe7ab
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-08 14:50:57 +0530

    filter

--- Array/Array7.js
+++ Array/Array7.js
@@ -1,1 +1,59 @@
 var language = ["hindi","english","marathi","gujarati","french","bengali","urdu"];
+
+
+
+const deleteLang = (langName)=> language.filter((lang)=> lang != langName)
+
+console.log(deleteLang("french"));
+
+
+
+
+// var x = deleteLang("french")
+
+// console.log(x);
+
+
+
+// const deleteLang = (langName)=>{
+
+//     return language.filter((lang) => lang != langName)
+// }
+
+// var x = deleteLang("marathi")
+// console.log(x); 
+
+
+
+
+
+// const deleteLang = (langName)=>{
+
+
+//     return language.filter((lang)=>{
+//         return lang!== langName;
+//     })
+// }
+// var x = deleteLang("marathi");
+// console.log(x);
+
+
+
+
+// const deleteLang = (langName)=>{
+
+
+//     var x = language.filter((lang)=>{
+//         return lang!== langName;
+//     })
+
+//     return x;
+
+// }
+
+
+// var deleteData = "gujarati"
+
+
+// var x = deleteLang(deleteData)
+// console.log(x);
